99c464f0cec9688bf471c884b6fc8fbd883f7594
sancarbar <sancarbar@gmail.com>
2020-05-06 19:01:53 -0500

    2. Added code to capture image.

--- app/src/main/java/com/android/meetup/bogota/mlvsion/MainActivity.kt
+++ app/src/main/java/com/android/meetup/bogota/mlvsion/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.android.meetup.bogota.mlvsion
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,11 +15,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener { dispatchTakePictureIntent() }
+    }
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
         }
     }
 
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            photo.setImageBitmap(imageBitmap)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
 }
